862a7793caf629223d0186292a3bd2f086c92706
DivyanshuRazz07 <125341260+DivyanshuRazz07@users.noreply.github.com>
2023-02-21 00:10:09 +0530

    Create code 11.c

---
+++ code 11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d%d",&a,&b);
+    c=a;
+    for(d=1;d<b;d++)
+    {
+        c=c*a;
+    }
+    printf("%d",c);
+    return 0;
+}
+Footer
+© 2023 GitHub, Inc.
