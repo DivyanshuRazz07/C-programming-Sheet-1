9934a4af6a0423a721ecbaf1869e044332c496df
DivyanshuRazz07 <125341260+DivyanshuRazz07@users.noreply.github.com>
2023-02-20 23:26:27 +0530

    Create code 4.c

---
+++ code 4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int length,breadth,perimeter;
+    scanf("%d%d",&length,&breadth);
+    perimeter=2*(length+breadth);
+    printf("%d",perimeter);
+    return 0;
+}
+Footer
+© 2023 GitHub, Inc.
